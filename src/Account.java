4a46c94bd62e4faa6e8a81bcf31477734f034a1f
Hoang Xuan Dieu <25021672@vnu.edu.vn>
2026-03-10 16:45:44 +0700

    add account feature

---
+++ src/Account.java
@@ -0,0 +1,9 @@
+public class Account {
+    private String id, name;
+    private double Balance;
+
+    public Account(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}
